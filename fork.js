14d24d088e3ae86189bf5824701c0217276acf6a
Ruslan <ruslanmuratbekov2004@gmail.com>
2023-12-30 20:57:06 +0300

    закончил основную логику

--- fork.js
+++ fork.js
@@ -2,21 +2,50 @@ const fs = require('node:fs')
 const path = require('path');
 
 process.on('message', (message) => {
-	const init = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-	const MEMORY = +process.execArgv[0].split('=')[1];
+	const restrictions_memory = message?.memory;
+	const pathFiles = message?.pathFiles;
 
-	const readStream = fs.createReadStream('text.txt', {highWaterMark: MEMORY})
-	const writeStream = init.map((filename) => fs.createWriteStream(path.join(__dirname, 'utils', filename), {highWaterMark: MEMORY}))
+	if (!restrictions_memory || !pathFiles) {
+		return process.exit();
+	}
+
+	const readStream = fs.createReadStream(pathFiles.inputFile, {highWaterMark: restrictions_memory.max_size_read});
+	const writeStreams = {};
+
+	function getFileName(word) {
+		const firstLetter = word.charAt(0).toLowerCase();
+		return `${firstLetter}.txt`
+	}
 
 	readStream.on('data', (chunk) => {
-		const stringArray = chunk.toString().split(' ');
-		stringArray.forEach((item) => {
-			const word = item.at(0).toUpperCase();
-			fs.writeFile(`./utils/${word}.txt`, item, console.log)
+		const wordArray = chunk.toString().split(' ');
+		wordArray.forEach((word) => {
+			const fileName = getFileName(word);
+			if (!writeStreams[fileName]) {
+				const res = path.join(__dirname, 'results', fileName)
+				writeStreams[fileName] = fs.createWriteStream(res, {
+					flags: 'a',
+					encoding: 'utf-8',
+					highWaterMark: restrictions_memory.max_size_write
+				})
+			}
+			writeStreams[fileName].write(` ${word}`)
 		})
 	})
 
 	readStream.on('end', () => {
-		process.send('Hehehehheh!');
+		for (const writeStream in writeStreams) {
+			writeStreams[writeStream].destroy();
+		}
+		readStream.destroy();
+		process.send('Сортировка файла закончено!');
+		process.exit();
 	});
 });
+
+// const init = [
+//   'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+//   'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+// ];
+// const initFileName = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+// const writeStream = init.map((filename) => fs.createWriteStream(path.join(__dirname, 'utils', filename), {highWaterMark: MEMORY}))
--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-const { fork } = require('child_process');
+const {fork} = require('child_process');
+const path = require("path");
 const MAX_MEMORY_SIZE = 500 * 1024 * 1024; // кол-во ОЗУ которую мы выделили для fork ;)
+const MAX_MEMORY_SIZE_READ = MAX_MEMORY_SIZE;
+const MAX_MEMORY_SIZE_WRITE = 15 * 1024 * 1024;
 
-const child = fork('fork.js', [], {
-	execArgv: [`--max-old-space-size=${MAX_MEMORY_SIZE}`] // ограничиваем до 500 mb
-});
+const child = fork('fork.js', []);
 
 child.on('message', (message) => {
 	console.log('Message from child:', message);
 });
 
-child.send('Hello from parent!');
+child.send({
+	memory: {
+		max_size: MAX_MEMORY_SIZE,
+		max_size_read: MAX_MEMORY_SIZE_READ,
+		max_size_write: MAX_MEMORY_SIZE_WRITE,
+	},
+	pathFiles: {
+		inputFile: path.join(__dirname, 'text.txt'),
+		outputFile: path.join(__dirname, 'utils'),
+	}
+});
 
 // // Условие:
 // // Имеется файл размером 1 тб, состоящий из строк.
